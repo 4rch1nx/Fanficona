38e39037827a8a119909034594115f7b6acff1c6
zen4xx <yarikshcher@gmail.com>
2025-08-24 01:34:46 +0300

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include "tiny_engine.h"
+#include <thread>
 
 int main()
 { 
-    auto engine = Tiny_engine("myapp", 800, 600, "test", TINY_ENGINE_MAX_MSAA_QUALITY, 2, true);
+    uint8_t max_threads = std::thread::hardware_concurrency();
+    uint8_t thread_count = 1;
+
+    if (max_threads > 2)
+        thread_count = max_threads - 2;
+
+    auto engine = Tiny_engine("Fanficona", 800, 600, "Fanficona", TINY_ENGINE_MAX_MSAA_QUALITY, thread_count, true);
 
     while(engine.isWindowOpen())
     {
